dc67f977d58446ba7f446917e3021313e37ba854
Parshva001 <dodhiaparshva@gmail.com>
2023-12-05 10:35:21 +0300

    added new functions

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 window.addEventListener('load', () =>{
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
+    const list_el = document.querySelector("#tasks");
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -17,7 +20,13 @@ window.addEventListener('load', () =>{
         task_content_el.classList.add("content");
         // task_content_el.innerText = task;
 
-        task_el.appendChild(task_content_el)
+        task_el.appendChild(task_content_el);
+
+        const task_input_el = document.createElement("input");
+        task_input_el.classList.add("text");
+        task_input_el.type = "text";
+        task_input_el.value = task;
+        task_input_el.setAttribute("readonly", "readonly");
 
         task_content_el.appendChild(task_input_el);
 
@@ -54,9 +63,6 @@ window.addEventListener('load', () =>{
                 task_edit_el.innerText = "Edit";
             }
         });
-        task_delete_el.addEventListener('click', () => {
-            list_el.removeChild(task_el);  
-        });
-    });
+        
 });
-
+});git 
